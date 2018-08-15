34006a351ddbdb223c36142d07dcde0ae0e3e7b7
williehwc <williehwc@gmail.com>
2018-08-15 10:11:45 -0400

    Move control mapping preferences to their own screen

--- app/src/main/java/com/atelieryl/wonderdroid/Prefs.java
+++ app/src/main/java/com/atelieryl/wonderdroid/Prefs.java
@@ -1,11 +1,16 @@
 
 package com.atelieryl.wonderdroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class Prefs extends PreferenceActivity {
+    PreferenceActivity activity = this;
+    boolean goBack = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +18,27 @@ public class Prefs extends PreferenceActivity {
 
         EditTextPreference override = (EditTextPreference)findPreference("emu_overriderompath");
         //getActionBar().setIcon(android.R.color.transparent);
+
+        findPreference("mapcontrols").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                goBack = true;
+                activity.setPreferenceScreen(null);
+                activity.addPreferencesFromResource(R.layout.mapcontrols);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (goBack) {
+            Intent intent = getIntent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            finish();
+            startActivity(intent);
+        } else {
+            finish();
+        }
     }
 }
