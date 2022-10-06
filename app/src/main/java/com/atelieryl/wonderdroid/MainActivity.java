57142c089663e0c8b55c18027732dc9012b161ec
Willie Chang <williehwc@gmail.com>
2022-10-05 22:09:49 -0500

    Load state warning

--- app/src/main/java/com/atelieryl/wonderdroid/MainActivity.java
+++ app/src/main/java/com/atelieryl/wonderdroid/MainActivity.java
@@ -60,6 +60,8 @@ public class MainActivity extends BaseActivity {
 
     private String packageName = "com.atelieryl.wonderdroid"; // Will be checked and replaced automatically if different
 
+    private boolean showStateWarning = true;
+
     private int currentBackupNo = 0;
 
     private final int maxBackupNo = 4;
@@ -101,6 +103,8 @@ public class MainActivity extends BaseActivity {
         packageName = getPackageName();
 
         portrait = prefs.getString("orientation", "landscape").equals("portrait");
+
+        showStateWarning = !prefs.getBoolean("hidestatewarning", false);
 
         // https://stackoverflow.com/questions/21724420
         final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -226,8 +230,25 @@ public class MainActivity extends BaseActivity {
                 return true;
 
             case R.id.main_savestate:
+                updateStateMenuTitles();
+                return true;
             case R.id.main_loadstate:
                 updateStateMenuTitles();
+                if (showStateWarning) {
+                    AlertDialog.Builder builder;
+                    builder = new AlertDialog.Builder(this);
+                    builder.setMessage(getResources().getString(R.string.statewarning))
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    showStateWarning = false;
+                                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+                                    SharedPreferences.Editor editor = prefs.edit();
+                                    editor.putBoolean("hidestatewarning", true);
+                                    editor.commit();
+                                }
+                            })
+                            .show();
+                }
                 return true;
             case R.id.load_a1:
                 loadState(-1);
